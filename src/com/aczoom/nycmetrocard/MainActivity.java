510f9c99ee318b60adc825b8612bd28aa2532f1b
Avinash <avinash@aczoom.com>
2014-07-23 20:17:01 -0400

    Add zoom capability and handle orientation changes
    Allow pinch-to-zoom for the app pages.
    Handle screen orientation change correctly when on the Help page.

--- src/com/aczoom/nycmetrocard/MainActivity.java
+++ src/com/aczoom/nycmetrocard/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,17 +14,22 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 /** HTML5 web page wrapped as an Android App.
- * http://developer.android.com/guide/webapps/webview.html
+ *
+ * Open Source: https://code.google.com/p/nyc-metrocard-refill/
  */
 
 @SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends Activity
 {
-   
-   private static final String UAAGENT = "NYCMetroCard";
-
    // For logging and debugging purposes
    private static final String TAG = "MainActivity";
+
+   // Custom User Agent - this is the suffix with version number
+   private static final String USER_AGENT = "NYCMetroCard";
+
+   private static final String ROOT_URL = "file:///android_asset/www/";
+   private static final String INDEX_URL = ROOT_URL + "index.html";
+   private static final String HELP_URL = ROOT_URL + "page.html";
 
    private WebView mWebView = null;
 
@@ -35,21 +41,41 @@ public class MainActivity extends Activity
 
       mWebView = (WebView)findViewById(R.id.webview);
 
+      // Web pages used in this app use JavaScript.
       mWebView.getSettings().setJavaScriptEnabled(true);
 
+      // HTML5 localStorage used to save form state.
+      // This uses internal storage, so requires no special Android permissions.
       mWebView.getSettings().setDomStorageEnabled(true);
 
+      // Allow pinch-to-zoom, but don't show the on-screen zoom controls.
+      Utils.EnableZoomWithoutControls(mWebView);
+
+      // Override URLs clicked on the pages to handle internal vs external links.
       mWebView.setWebChromeClient(new WebChromeClient());
       mWebView.setWebViewClient(new MyWebViewClient());
 
+      // Use a custom User Agent string, so JavaScript in the pages can detect
+      // if running under this app, or on a web browser.
       String ua = mWebView.getSettings().getUserAgentString();
       String version = Utils.getAppVersion(this);
-      String customUA = ua + " " + UAAGENT + "/" + version;
+      String customUA = ua + " " + USER_AGENT + "/" + version;
       mWebView.getSettings().setUserAgentString(customUA);
-      
-      mWebView.loadUrl("file:///android_asset/www/index.html");
+      Log.d(TAG, "Using UA: " + customUA);
+
+      // If this is a screen orientation change, etc, don't load any URLs
+      // otherwise, load the start page.
+      if (savedInstanceState == null) {
+         mWebView.loadUrl(INDEX_URL);
+      }
    }
 
+   /*
+    * When Android Back Button is pressed, see if we need to go back in
+   // our app's internal pages, or to exit the app, etc.
+    * (non-Javadoc)
+    * @see android.app.Activity#onBackPressed()
+    */
    @Override
    public void onBackPressed() {
       // Check if there's history
@@ -62,14 +88,30 @@ public class MainActivity extends Activity
       // system behavior (probably exit the activity)
       super.onBackPressed();
    }
-   
+
+   /*
+    * Handle orientation change, save webview state (saves the url and page history)
+    * Necessary to allow help.html page to remain on screen on orientation change.
+    */
+   @Override
+   protected void onSaveInstanceState(Bundle outState) {
+      super.onSaveInstanceState(outState);
+      mWebView.saveState(outState);
+   }
+
+   @Override
+   protected void onRestoreInstanceState(Bundle savedInstanceState)
+   {
+     super.onRestoreInstanceState(savedInstanceState);
+     mWebView.restoreState(savedInstanceState);
+   }
+
    /* Options Menu
     * This may not be used - the manifest may set theme to use
     * full screen without the action bar, since that dislays the page better.
     * Leaving code here just in case it is needed later.
     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
     */
-
    @Override
    public boolean onCreateOptionsMenu(Menu menu) {
       // Use this later if needed:
@@ -79,7 +121,7 @@ public class MainActivity extends Activity
       //   android:theme="@android:style/Theme.NoTitleBar.Fullscreen">
       return true;
    }
-   
+
    @Override
    public boolean onOptionsItemSelected(MenuItem item) {
        // Handle item selection
@@ -91,28 +133,27 @@ public class MainActivity extends Activity
                return super.onOptionsItemSelected(item);
        }
    }
-   
+
+   /*
+    * Display help page.
+    */
    private void showHelp() {
-      mWebView.loadUrl("file:///android_asset/www/help.html");      
+      mWebView.loadUrl(HELP_URL);
    }
 
    /*
-    * Local class to override web HTML link navigation
+    * Local class to override user clicks on the app pages' HREF links
     */
    private class MyWebViewClient extends WebViewClient {
       @Override
       public boolean shouldOverrideUrlLoading(WebView view, String url) {
-         Log.d(TAG, "Got URL " + url);
-         // Got URL file:///android_asset/www/help.html
-         // Scheme: file
-         // Host: <empty>
+         // Example: URL file:///android_asset/www/help.html
+         //          Host: <empty>
          Uri uri = Uri.parse(url);
          String host = uri.getHost();
-         Log.d(TAG, "Got Scheme " + uri.getScheme());
-         Log.d(TAG, "Got Host " + uri.getHost());
 
-         if (host.equals("")) {
-            // This is my web site, so do not override; let my WebView load the page
+         if (TextUtils.isEmpty(host)) {
+            // Local files, so do not override; let my WebView load the page
             return false;
          }
          // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
--- src/com/aczoom/nycmetrocard/Utils.java
+++ src/com/aczoom/nycmetrocard/Utils.java
@@ -1,13 +1,17 @@
 package com.aczoom.nycmetrocard;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ZoomButtonsController;
 
 /**
  * Shared utility functions used by multiple activities.
- * 
+ *
  * @author avinash
  *
  */
@@ -16,7 +20,6 @@ public enum Utils {
 
    // For logging and debugging purposes
    private static final String TAG = "Utils";
-
 
    // Get the app package info with version name, version code, etc.
    public static PackageInfo getAppPackageInfo(Context context)
@@ -33,12 +36,55 @@ public enum Utils {
       }
       return pInfo;
    }
-   
+
    // Get app version string
    public static String getAppVersion(Context context)
    {
       PackageInfo pInfo = Utils.getAppPackageInfo(context);
       return pInfo != null ? pInfo.versionName : "0.0.0";
    }
-   
+
+
+   /**
+    * Turn on Zoom (pinch-to-zoom), but disable on-screen zoom buttons for WebView.
+    * http://stackoverflow.com/questions/5125851/enable-disable-zoom-in-android-webview/14751673#14751673
+    *
+    * App runs on API 10 or lower which has no easy way to disable the
+    *  displayed on-screen + - zoom controls.
+    * mWebView.getSettings().setBuiltInZoomControls(false); // this is fine
+    * mWebView.getSettings().setDisplayZoomControls(false); // API 11 only
+    * So the code below is a workaround that seems to work fine on 2.3.3 tablets.
+    */
+   @TargetApi(11)
+   public static void EnableZoomWithoutControls(final WebView webview) {
+      webview.getSettings().setSupportZoom(true);
+      webview.getSettings().setBuiltInZoomControls(true);
+      webview.getSettings().setUseWideViewPort(true);
+      // WideViewPort essentially zooms page and uses horizontal scroll bars.
+      // WideViewPort necessary to avoid garbling table header row and all columns on
+      // Android 2.3.7 versions. Not necessary on newer Android.
+      // Provides for much better interaction, similar to normal web browser zooming.
+
+      // Use the API 11+ calls to disable the controls.
+      // Use a separate class to obtain 1.6 compatibility (avoid crashes on older systems).
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+         new Runnable() {
+            public void run() {
+               webview.getSettings().setDisplayZoomControls(false);
+            }
+         }.run();
+      } else {
+         try {
+            ZoomButtonsController zoom_control;
+            zoom_control = ((ZoomButtonsController) webview.getClass().getMethod("getZoomButtonsController").invoke(webview, (Object[]) null));
+            zoom_control.getContainer().setVisibility(View.GONE);
+         }
+         catch (Exception e) {
+            Log.d(TAG, "Failed to use ZoomButtonsController " + e);
+            // e.printStackTrace();
+         }
+      }
+   }
+
+
 }
